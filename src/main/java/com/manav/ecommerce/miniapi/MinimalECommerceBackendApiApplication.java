0e585e1c597e6f995fff7a9ccf7283df3aedd0ae
ManavPratap <manav.24bcs10126@sst.scaler.com>
2026-01-19 16:41:15 +0530

    initial commit

---
+++ src/main/java/com/manav/ecommerce/miniapi/MinimalECommerceBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.manav.ecommerce.miniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalECommerceBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalECommerceBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manav/ecommerce/miniapi/MinimalECommerceBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manav.ecommerce.miniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalECommerceBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
