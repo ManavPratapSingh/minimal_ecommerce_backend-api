9fde01b602b8e304fc0c7001cba210d55b047e93
ManavPratap <manav.24bcs10126@sst.scaler.com>
2026-01-19 16:56:54 +0530

    Added the product controller with the basic routes

---
+++ src/main/java/com/manav/ecommerce/miniapi/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.manav.ecommerce.controller;
+
+import com.manav.ecommerce.model.Product;
+import com.manav.ecommerce.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product, @RequestParam Long categoryId) {
+        Product createdProduct = productService.addProduct(product, categoryId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+}
